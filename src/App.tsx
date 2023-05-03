4191c141a60bc5367be4d6ed50be9821312ece83
Eyup Fidan <info@eyupfidan.com>
2023-05-03 15:24:29 +0300

    Update ESLint and Prettier configurations, add PostCSS configuration, and update HomeMeta component metadata in files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import HomeMeta from "./components/HomeMeta"
-
+import HomeMeta from "./components/HomeMeta";
 
 function App() {
-
   return (
-    <HomeMeta/>
-)
+    <div className="w-screen h-screen bg-slate-50		">
+      <HomeMeta />
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold underline text-center text-red-500">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
